d0eebfd2ce625be9165b6c3fcd874bf12dd5effc
Mattia Vandi <mattia.vandi@studio.unibo.it>
2019-03-30 10:38:44 +0100

    Implemented Job1 in Spark

--- src/main/scala/it/unibo/bd18/stacklite/Job1.scala
+++ src/main/scala/it/unibo/bd18/stacklite/Job1.scala
@@ -1,9 +1,37 @@
 package it.unibo.bd18.stacklite
 
-import org.apache.spark.SparkConf
+import java.util.{Calendar, Date}
 
+import org.apache.spark.{HashPartitioner, SparkConf}
+
+/**
+  * Determine the five tags that received the highest sum of scores for each
+  * year-month pair (sorted in descending order);
+  */
 object Job1 extends StackliteApp {
 
+  import implicits._
+  import it.unibo.bd18.util.implicits._
+
   override protected[this] val conf: SparkConf = new SparkConf().setAppName("Job1")
+
+  val result = questionsRDD.keyBy(_.id)
+    .partitionBy(new HashPartitioner(sc.coreCount))
+    .join(questionTagsRDD.keyBy(_.id))
+    .map(x => (getYearMonthPair(x._2._1.creationDate), (x._2._2.tag, x._2._1.score)))
+    .groupByKey()
+    .mapValues(_.toRDD
+      .groupBy(_._1)
+      .mapValues(_.map(_._2).sum)
+      .sortBy(-_._2)
+      .map(_._1)
+      .take(5)
+      .toList)
+
+  private def getYearMonthPair(d: Date): (Int, Int) = {
+    val c = Calendar.getInstance()
+    c.setTime(d)
+    (c.get(Calendar.YEAR), c.get(Calendar.MONTH) + 1)
+  }
 
 }

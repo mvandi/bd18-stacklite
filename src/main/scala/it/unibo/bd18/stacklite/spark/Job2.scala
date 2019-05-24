9273ba5529653aa31891db11143698f9bd399bd5
Mattia Vandi <mattia.vandi@studio.unibo.it>
2019-05-24 17:24:56 +0200

    Discretized participation in Job2 Spark

--- src/main/scala/it/unibo/bd18/stacklite/spark/Job2.scala
+++ src/main/scala/it/unibo/bd18/stacklite/spark/Job2.scala
@@ -11,17 +11,18 @@ object Job2 extends StackliteSQLApp {
   import it.unibo.bd18.stacklite.C.dates
   import it.unibo.bd18.stacklite.Utils
   import org.apache.hadoop.fs.Path
-  import org.apache.spark.sql.functions.{count, sum, when}
+  import org.apache.spark.sql.Column
+  import org.apache.spark.sql.functions._
   import spark.implicits._
 
-  def d(d: Date): String = Utils.toString(d)
+  @inline def d(d: Date): String = Utils.toString(d)
 
   val resultPath = args(0)
   Utils.deleteIfExists(fs, true, new Path(resultPath))
 
-  questionsDF
+  val baseDF = questionsDF
     .where(($"creationDate" between(d(dates.startDate), d(dates.endDate)))
-      && ($"deletionDate" isNull))
+        && ($"deletionDate" isNull))
     .join(questionTagsDF, "id")
     .withColumn("open", when($"closedDate" isNull, 1) otherwise 0)
     .groupBy("name")
@@ -29,16 +30,32 @@ object Job2 extends StackliteSQLApp {
       sum("open") as "openQuestions",
       count("*") as "questionCount",
       sum("answerCount") as "totalAnswers")
-    .orderBy($"name" asc)
+    .cache()
+
+  baseDF.crossJoin(baseDF
+    .select(min("totalAnswers") as "minParticipation",
+            max("totalAnswers") as "maxParticipation"))
     .select(
       $"name",
       $"openQuestions",
       $"questionCount",
       $"totalAnswers",
-      ($"openQuestions" / $"questionCount") * 100 as "openingRate",
-      ($"totalAnswers" / $"questionCount") as "averageParticipation")
+      ($"openQuestions" / $"questionCount") as "openingRate",
+      ($"totalAnswers" / $"questionCount") as "averageParticipation",
+      discretize($"totalAnswers", $"minParticipation", $"maxParticipation") as "participation")
     .write.parquet(resultPath)
 
   //resultDF.explain(extended = true)
+
+  private[this] def discretize(x: Column, min: Column, max: Column): Column = {
+    val lowThreshold = 1.0 / 3.0
+    val highThreshold = 2.0 / 3.0
+
+    val normalized = (x - min) / (max - min)
+
+    when(normalized < lowThreshold, "LOW")
+      .when(normalized > highThreshold, "HIGH")
+      .otherwise("MEDIUM")
+  }
 
 }

a972e85d31d51f7fd9532ea73e113bc5ef11ce77
martynha <38749739+martynha@users.noreply.github.com>
2019-05-27 12:16:39 +0200

    Second Job in SparkSQL (#2)
    * Implemented second job join
    
    * Implemented ClosingRateWithAverageParticipation job
    
    * Implemented Main
    
    * First implementation of second job
    
    * Implemented second Job in SparkSQL
    
    * Implemented and tested second job in sparkSQL

--- src/main/scala/it/unibo/bd18/stacklite/spark/Job2.scala
+++ src/main/scala/it/unibo/bd18/stacklite/spark/Job2.scala
@@ -1,12 +1,6 @@
 package it.unibo.bd18.stacklite.spark
 
-import org.apache.spark.SparkConf
-
 object Job2 extends StackliteSQLApp {
-
-  override protected[this] val conf: SparkConf = new SparkConf().setAppName("Job2")
-
-  import java.util.Date
 
   import it.unibo.bd18.stacklite.C.dates
   import it.unibo.bd18.stacklite.Utils
@@ -14,31 +8,22 @@ object Job2 extends StackliteSQLApp {
   import org.apache.spark.sql.functions.{count, sum, when}
   import spark.implicits._
 
-  def d(d: Date): String = Utils.toString(d)
 
   val resultPath = args(0)
   Utils.deleteIfExists(fs, true, new Path(resultPath))
 
-  questionsDF
-    .where(($"creationDate" between(d(dates.startDate), d(dates.endDate)))
-      && ($"deletionDate" isNull))
+  val resultDF = questionsDF
+    .where($"creationDate" between(Utils.toString(dates.startDate), Utils.toString(dates.endDate)) and ($"deletionDate" isNull))
     .join(questionTagsDF, "id")
-    .withColumn("open", when($"closedDate" isNull, 1) otherwise 0)
+    .withColumn("openQuestions", when($"closedDate".isNull, 1) otherwise 0)
     .groupBy("name")
-    .agg(
-      sum("open") as "openQuestions",
-      count("*") as "questionCount",
-      sum("answerCount") as "totalAnswers")
-    .orderBy($"name" asc)
-    .select(
-      $"name",
-      $"openQuestions",
-      $"questionCount",
-      $"totalAnswers",
+    .agg(count("*") as "questionCount",
+      sum("answerCount") as "totalAnswers",
+      sum("openQuestions") as "openQuestions")
+    .orderBy("name")
+    .select($"name", $"openQuestions", $"questionCount", $"totalAnswers",
       ($"openQuestions" / $"questionCount") * 100 as "openingRate",
-      ($"totalAnswers" / $"questionCount") as "averageParticipation")
+      $"totalAnswers" / $"questionCount" as "averageParticipation")
     .write.parquet(resultPath)
-
-  //resultDF.explain(extended = true)
 
 }
